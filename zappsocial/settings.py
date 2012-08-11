1c786528a5516c06717dcbbca02e0547d225a2cb
Adam Bednarek <adamjb51@gmail.com>
2012-08-11 15:18:34 -0400

    Add the Quiz app in django

--- zappsocial/settings.py
+++ zappsocial/settings.py
@@ -114,6 +114,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    'quiz',
 )
 
 # A sample logging configuration. The only tangible logging
