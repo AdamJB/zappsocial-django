80a127f8fd5e1ccbd62b0a24c32c55a8082b7d31
Adam Bednarek <adamjb51@gmail.com>
2012-09-13 14:12:37 -0400

    Add some basic view / urls for listing shows.
    Also including basic stubing / scaffolding of basic views.

--- quiz/views.py
+++ quiz/views.py
@@ -1,1 +1,32 @@
-# Create your views here.
+from django.template import Context, loader
+from django.http import HttpResponse
+from models import *
+
+# TOOD: Move this to a subfolder views/quiz.py
+
+def index(request):
+  shows_list = Show.objects.all().order_by('title')
+  template = loader.get_template('quiz/index.html')
+  c = Context({
+      'shows_list': shows_list,
+  })
+  return HttpResponse(template.render(c))
+
+# TOOD: (These are generic stubs)
+
+def generate(request, quiz_type, id):
+  #TODO: quizGeneration Logic
+  if quiz_type == "show":
+    show = Show.objects.all().filter(pk=id)
+    if show:
+      return HttpResponse("Generate Show quiz for: %s " % show[0].title)
+    else:
+      return HttpResponse("Invalid show to create quiz for")
+  #TODO: Can only have active 1 quiz per show/genre at a time?
+  return HttpResponse("Can't generate a quiz with no info")
+
+def detail(request, quiz_id):
+  return HttpResponse("Start Quiz? / Results / Current Quizes")
+
+def question(request, quiz_id, question_id):
+  return HttpResponse("Answer This question? / Answered already?")
--- zappsocial/urls.py
+++ zappsocial/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -12,6 +14,13 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^quiz/$', 'quiz.views.index'),
+    url(r'^quiz/generate/(?P<quiz_type>\w+)/(?P<id>\d+)$', 'quiz.views.generate'),
+    #TODO:
+    url(r'^quiz/(?P<quiz_id>\d+)/$', 'quiz.views.detail'),
+    url(r'^quiz/(?P<quiz_id>\d+)/(?P<question_id>\d+)/$', 'quiz.views.question'),
+    url(r'^quiz/(?P<quiz_id>\d+)/results/$', 'quiz.views.detail'),
 )
+
+urlpatterns += staticfiles_urlpatterns()
